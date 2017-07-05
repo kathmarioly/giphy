3534f16343c773fdf9ac2362617ca345dde7f4bd
kathmarioly <katerinemarioly@gmail.com>
2017-07-05 10:08:28 -0400

    Solving syntax errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,40 @@ $(document).ready(function(){
 		data.forEach(function(element){
 			gif = element.images.downsized_large.url;
 			url = element.bitly_gif_url;
-			$("elementos").append(armarTemplate(gif , url));
-		})
+			$("#elementos").append(armarTemplate(gif , url));
+		});
 	}
-})
+
+
+/*Se arma la estructura que se mostrará en html, con los datos que hemos 
+solicitado anteriormente*/
+var armarTemplate = function(gif,url){
+	var t = "<div class='elemento'><img src='" + gif + "'/><a href ='" + url +"'>Ver más</a></div>"
+	return t;
+}
+
+var ajaxGif = function(gif){
+	$.ajax({
+		url: 'http://api.giphy.com/v1/gifs/search',
+		type: 'GET',
+		dataType: 'json',
+		data: {
+			q :gif,
+			api_key: 'dc6zaTOxFJmzC'
+		}
+	})
+	.done(function(response) {
+		console.log(response);
+		dibujarGifs(response.data);
+	})
+	.fail(function() {
+		console.log("error");
+	});	
+}
+$("#buscar-gif").click(function(event){
+	console.log("Entro");
+	$("elementos").empty();
+	var gif = $("#gif-text").val();
+	ajaxGif(gif);	
+});
+});

99d08aba4bc29f151d34abf359bcee14c404aa6f
kathmarioly <katerinemarioly@gmail.com>
2017-07-06 17:51:16 -0400

    Finished details JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,40 @@
-$(document).ready(function(){
-	var dibujarGifs =function(data){
+$(document).ready(function() {
+	var dibujarGifts = function(data){
 		var gif = "";
 		var url = "";
 		data.forEach(function(element){
 			gif = element.images.downsized_large.url;
 			url = element.bitly_gif_url;
-			$("#elementos").append(armarTemplate(gif , url));
+			$("#elementos").append(armarTemplate(gif, url));
 		});
 	}
-
-
-/*Se arma la estructura que se mostrará en html, con los datos que hemos 
-solicitado anteriormente*/
-var armarTemplate = function(gif,url){
-	var t = "<div class='elemento'><img src='" + gif + "'/><a href ='" + url +"'>Ver más</a></div>"
-	return t;
-}
-
-var ajaxGif = function(gif){
-	$.ajax({
-		url: 'http://api.giphy.com/v1/gifs/search',
-		type: 'GET',
-		dataType: 'json',
-		data: {
-			q :gif,
-			api_key: 'dc6zaTOxFJmzC'
-		}
+	var armarTemplate = function(gif, url){
+		var t = "<div class='elemento'><img src='" + gif + "'/><a href='" + url + "'>Ver más</a></div>";
+		console.log(t);
+		return t;
+	}
+	var ajaxGif = function(gif){
+		$.ajax({
+			url : 'http://api.giphy.com/v1/gifs/search',
+			type : 'GET',
+			datatype : 'json',
+			data : {
+				q : gif,
+				api_key : 'dc6zaTOxFJmzC'
+			}
+		})
+		.done(function(response){
+			console.log(response);
+			dibujarGifts(response.data);
+		})
+		.fail(function(){
+			console.log("error");
+		})
+	}
+	$("#buscar-gif").click(function(event){
+		console.log("Entro");
+		$("#elementos").empty();
+		var gif = $("#gif-text").val();
+		ajaxGif(gif);
 	})
-	.done(function(response) {
-		console.log(response);
-		dibujarGifs(response.data);
-	})
-	.fail(function() {
-		console.log("error");
-	});	
-}
-$("#buscar-gif").click(function(event){
-	console.log("Entro");
-	$("elementos").empty();
-	var gif = $("#gif-text").val();
-	ajaxGif(gif);	
-});
 });
